0594c5a62a29ed0dc7dd59a66b96d8906908e243
Ala-Aster <spring-mage@outlook.com>
2026-08-20 21:56:12 +0900

    Update characters.js

--- characters.js
+++ characters.js
@@ -79,6 +79,8 @@ const CHARACTER_DATA = [
   { id: "c021", name: "タチアナ",             kana: "たちあな",                 cost: "2.0", hasRare: 1 },
   { id: "c022", name: "フィービー",           kana: "ふぃーびー",               cost: "2.0", hasRare: 2 },
   { id: "c023", name: "レム",                 kana: "れむ",                     cost: "2.0", hasRare: 2 },
+  { id: "c023", name: "レア",                 kana: "れあ",                     cost: "2.0", hasRare: 0 },
+
   
 
   // --- コスト 1.5 ---
